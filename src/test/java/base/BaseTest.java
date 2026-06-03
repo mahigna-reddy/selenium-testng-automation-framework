ef4dc5b03ac16a18ada74ff5ce9ea7bc03c99a6a
mahigna-reddy <vemulamahigna@gmail.com>
2026-06-03 07:57:49 -0500

    Add BaseTest and TestData constants

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,40 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-features=PasswordLeakDetection");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/testdata/TestData.java
@@ -0,0 +1,23 @@
+package testdata;
+
+public class TestData {
+
+    public static final String BASE_URL = "https://www.saucedemo.com/";
+
+    public static final String STANDARD_USERNAME = "standard_user";
+    public static final String INVALID_USERNAME = "invalid_user";
+    public static final String PASSWORD = "secret_sauce";
+    public static final String INVALID_PASSWORD = "wrong_password";
+
+    public static final String FIRST_NAME = "Emma";
+    public static final String LAST_NAME = "Red";
+    public static final String POSTAL_CODE = "75070";
+
+    public static final String BACKPACK_PRODUCT = "Sauce Labs Backpack";
+    public static final String BIKE_LIGHT_PRODUCT = "Sauce Labs Bike Light";
+    public static final String BOLT_T_SHIRT_PRODUCT = "Sauce Labs Bolt T-Shirt";
+
+    public static final String LOGIN_ERROR_MESSAGE = "Username and password do not match";
+    public static final String ORDER_CONFIRMATION_MESSAGE = "Thank you for your order!";
+    public static final String SAUCEDEMO_DOMAIN = "saucedemo.com";
+}
